8c177deb19ee1bca1f46574c95f28a83a057f711
nikpapage23 <65818389+nikpapage23@users.noreply.github.com>
2021-05-22 00:36:58 +0300

    Update bookToreturn.java

--- Project-code-v0.1/bookToreturn.java
+++ Project-code-v0.1/bookToreturn.java
@@ -1,31 +1,35 @@
 import java.util.*;
+import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 
-class bookToreturn extends book
+class BookToReturn extends Book
 {
     private User User;
     private Date return_date;
     private PIN bookToreturn_pin;
-public bookToreturn(String idBook,String type,String author,String publisher,User user)
-{
-    super(idBook, type, author, publisher);
-    this.user = user;
 
-    this.bookToreturn_pin = PIN.generator_PIN();
-
-    this.return_date = new Date();
-
-    Calendar c = Calendar.getInstance();
-    
-    this.return_date = c.geTime();
-}
-
-public User getUser(){
-    return user;
-}
-
-public Date getReturnDate()
+    public bookToreturn(String idBook,String type,String author,String publisher,User user)
     {
-        return return_date;
+        super(idBook, type, author, publisher);
+        this.user = user;
+
+        this.bookToreturn_pin = PIN.generator_PIN();
+
+        this.return_date = new Date();
+
+        Calendar c = Calendar.getInstance();
+        
+        this.return_date = c.getTime();
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    public Date getReturnDate()
+    {
+            return return_date;
     }
 
     public int getBookToReturnPin()
@@ -33,10 +37,18 @@ public Date getReturnDate()
         return bookToreturn_pin;
     }
 
+    public boolean cmpReturnWithCurrentDate() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");  
+        Date date = new Date();
+        Date currdate = formatter.parse(date);
+        if (currdate.compareTo(return_date) == -1) {
+            return false;
+        }
+        else return true;
+    }
+
     public String getDetails()
     {
         return("\nUser name: " +this.user.getuName()+"\nPIN: " +this.bookToreturn_pin + "\nReturn Date: "+this.return_date); 
     }
-
-
 }
