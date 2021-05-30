7628c04178a14083124b86017954130679feed85
nikpapage23 <65818389+nikpapage23@users.noreply.github.com>
2021-05-30 19:29:37 +0300

    code update
    Updated MyBooks class: added "getMyBooksList()" method

--- Project-code-v0.2/MyBooks.java
+++ Project-code-v0.2/MyBooks.java
@@ -3,6 +3,10 @@ import java.util.*;
 public class MyBooks {
     private ArrayList<BorrowedBook> mybookslist = new ArrayList<BorrowedBook>(5);
 
+    public ArrayList<BorrowedBook> getMyBooksList() {
+        return mybookslist;
+    }
+
     public void addBorrowedBook(BorrowedBook b) {
         mybookslist.add(b);
     }
