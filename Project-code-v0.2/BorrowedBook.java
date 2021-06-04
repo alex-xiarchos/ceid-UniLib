23b248673fb6d8f46034a116bb8aabb6d1f4d543
ChristosBoulafentis <82766346+ChristosBoulafentis@users.noreply.github.com>
2021-06-04 17:27:07 +0300

    BorrowedBook Class
    Uploaded again with small changes in order not to delete the Code v0.1 version.

---
+++ Project-code-v0.2/BorrowedBook.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class BorrowedBook extends Book{
+
+    private User user;
+    private Date borrow_start_date, borrow_expire_date;
+	    
+    public BorrowedBook(String title, String category, String author, String publishing_house, String ISBN, String details, User user) {  
+        super(title, category, author, publishing_house, ISBN, details);
+        this.user = user;  
+        this.borrow_start_date = new Date();
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(borrow_start_date);
+        c.add(Calendar.DATE, 15);
+        this.borrow_expire_date = c.getTime();
+    }  
+
+
+    /*public String getIdBook(){
+        return idBook;
+    }*/
+
+    public User getUser(){
+        return user;
+    }
+
+    public Date getBorrowStartDate(){
+        return borrow_start_date;
+    }
+
+    public Date getBorrowExpireDate(){
+        return borrow_expire_date;
+    }
+
+
+    /*public void setIdBook(String idBook){
+        this.idBook = idBook;
+    }
+
+    public void setUser(int user){
+        this.user = user;
+    }
+
+    public void setBorrowStartDate(Date borrow_start_date){
+        this.borrow_start_date = borrow_start_date;
+    }
+
+    public void setBorrowExpireDate(Date borrow_expire_date){
+        this.borrow_expire_date = borrow_expire_date;
+    }*/
+
+    /* Συνάρτηση που επεκτείνει την ημερομηνία λήξης
+    του δανεισμού ενός βιβλίου κατά έναν αριθμό ημερών. */
+
+    public void expandExpireDate(int days) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(borrow_expire_date);
+        c.add(Calendar.DAY_OF_MONTH, days);
+        borrow_expire_date = c.getTime();
+    }
+}
